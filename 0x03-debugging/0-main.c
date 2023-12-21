5022622300b25422d5736d284600c271e33f1b3d
SaraSmimou <smimousara@gmail.com>
2023-12-21 13:02:23 -0800

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,12 @@
-// main.c file
 #include "main.h"
-
 /**
- * positive_or_negative - Checks if a number is positive, negative, or zero
- * @n: The number to be checked
- */
-void positive_or_negative(int n) {
-    if (n == 0) {
-        printf("The number %d is zero.\n", n);
-    } else if (n > 0) {
-        printf("The number %d is positive.\n", n);
-    } else {
-        printf("The number %d is negative.\n", n);
-    }
-}
-
-/**
- * main - Test function for positive_or_negative
- * Return: 0
- */
-int main(void) {
-    int i;
-    i = 0;
-    positive_or_negative(i);
-    return 0;
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
